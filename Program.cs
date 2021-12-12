a8f72b8aa0193e536a2602fb575e7e091d77ea21
BetterRage <benni.schmidties@gmail.com>
2021-12-12 19:31:22 +0100

    fix build

--- Program.cs
+++ Program.cs
@@ -7,14 +7,11 @@ using System.IO;
 using System.Linq;
 using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
-using Microsoft.Toolkit.Uwp.Notifications;
 
 namespace CryptoService
 {
     static class Program
     {
-
-
         static void Main()
         {
             string[] keys = ReadAPIKeysFromFile();
@@ -22,10 +19,13 @@ namespace CryptoService
             string[] cryptos = ReadCryptosFromFile();
             keyIndex %= keys.Length;
             string result = MakeAPICall(keys[keyIndex]).Result;
-            (string s, float p)[] cryptoData = GetCryptosFromAPIResult(result, cryptos);
-            foreach (var crypto in cryptoData)
+            IEnumerable<(string s, float p)> cryptoData = GetCryptosFromAPIResult(result, cryptos);
+            int length = cryptoData.Max(x => x.s.Length);
+            foreach (var (s, p) in cryptoData)
             {
-                Console.WriteLine(string.Format(crypto.s.PadRight(5, ' '), crypto.p));
+                Console.Write(s.PadRight(length + 3, ' '));
+                Console.Write(p);
+                Console.WriteLine("€");
             }
             Console.ReadKey();
 
@@ -35,7 +35,7 @@ namespace CryptoService
         {
             using FileStream configFile = File.OpenRead("config.txt");
             using StreamReader cryptoReader = new(configFile);
-            string[] cryptos = cryptoReader.ReadToEnd().Split("\n")[1].Split(" ");
+            string[] cryptos = cryptoReader.ReadLine().Split(" ");
             cryptoReader.Close();
             configFile.Close();
             return cryptos;
@@ -65,17 +65,17 @@ namespace CryptoService
             string keysIn = keysReader.ReadToEnd();
             keysFile.Close();
             keysReader.Close();
-            return keysIn.Split(Environment.NewLine);
+            return keysIn.Split(Environment.NewLine).Skip(1).ToArray();
         }
 
         static int ReadCurrentKeyIndexFromFile()
         {
-            FileStream configFile = File.OpenRead("config.txt");
+            FileStream configFile = File.OpenRead("keys.txt");
             using StreamReader keyIndexReader = new(configFile);
             int keyIndex = Convert.ToInt32(keyIndexReader.ReadLine());
             keyIndexReader.Close();
             configFile.Close();
-            configFile = File.OpenWrite("config.txt");
+            configFile = File.OpenWrite("keys.txt");
             using StreamWriter keyIndexWriter = new(configFile);
             keyIndexWriter.Write(++keyIndex);
             keyIndexWriter.Close();
@@ -84,26 +84,16 @@ namespace CryptoService
             return keyIndex;
         }
 
-        static (string, float)[] GetCryptosFromAPIResult(string apiResult, string[] cryptos)
+        static IEnumerable<(string, float)> GetCryptosFromAPIResult(string apiResult, string[] cryptos)
         {
             JObject data = JObject.Parse(apiResult);
             JArray cryptosIn = (JArray)data["data"];
-            List<JToken> filteredCryptos = new();
             foreach (JToken jToken in cryptosIn)
-            {
                 foreach (string crypto in cryptos)
-                {
                     if (crypto == (string)jToken["symbol"])
-                        filteredCryptos.Add(jToken);
-                }
-            }
-            (string s, float p)[] formatedCryptos = new (string s, float p)[filteredCryptos.Count];
-            for (int i = 0; i < filteredCryptos.Count; i++)
-            {
-                formatedCryptos[i].s = (string)filteredCryptos[i]["symbol"];
-                formatedCryptos[i].p = (float)filteredCryptos[i]["quote"]["EUR"]["price"];
-            }
-            return formatedCryptos;
+                        yield return
+                            ((string)jToken["name"],
+                            (float)jToken["quote"]["EUR"]["price"]);
         }
     }
 }
